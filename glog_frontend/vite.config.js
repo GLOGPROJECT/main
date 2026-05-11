94161c60ba575b5b29652295963fb9a98ceeca62
lee-seon-hyeok <khm970203@gmail.com>
2026-05-12 06:18:02 +0900

    Revert "피드 UI 개선 & 메인 화면 개선"

--- glog_frontend/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000, // 백엔드 .env의 FRONTEND_URL(http://localhost:3000)과 맞추기 위해 고정
-  },
-});
